c7d065c52f6b2b0bcfbf0123c403820072df7d5d
Adrian Schoenig <adrian@schoenig.me>
2019-10-27 12:00:00 +0100

    Remove test target from Package to simplify for users

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -4,15 +4,10 @@ import PackageDescription
 let package = Package(
     name: "FlexibleDiff",
     products: [
-        .library(name: "FlexibleDiff", targets: ["FlexibleDiff"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+        .library(name: "FlexibleDiff", targets: ["FlexibleDiff"])
     ],
     targets: [
-        .target(name: "FlexibleDiff", path: "FlexibleDiff"),
-        .testTarget(name: "FlexibleDiffTests", dependencies: ["FlexibleDiff", "Quick", "Nimble"], path: "FlexibleDiffTests"),
+        .target(name: "FlexibleDiff", path: "FlexibleDiff")
     ],
     swiftLanguageVersions: [.v5]
 )
